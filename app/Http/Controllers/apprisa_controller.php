c71b8b6ee6594851043dd6d13b15140828c32b12
Brandon Guerra Espinoza <subgerencia-sistemas@asicofi.com>
2023-12-26 12:34:55 -0600

    Fix: Geocerca

--- app/Http/Controllers/apprisa_controller.php
+++ app/Http/Controllers/apprisa_controller.php
@@ -318,7 +318,7 @@ class apprisa_controller extends Controller
 
                 if ($geofence_validate == false || $geofence_validate = null) {
                     switch ($request->type) {
-                        case 'circle':
+                        case 'Circle':
                             $drawing = apprisa_drawing_modes::where('mode', $request->type);
                             $create_geofence = apprisa_geofences::insert([
                                 "geofence_name" => $request->name,
@@ -343,7 +343,7 @@ class apprisa_controller extends Controller
                             }
                             break;
 
-                        case 'polygon':
+                        case 'Polygon':
                             # code...
                             break;
                     }

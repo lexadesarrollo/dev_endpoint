5d4087e1ff8cb3a552d8a8cb3f2d8d4c0af5db07
Eduardo Martinez Tadeo <infraestructuralexaconsulting@gmail.com>
2024-01-02 09:27:10 -0600

    Feat: Nuevas funciones API Censo
    Nuevas funciones en el controlador de CensoApp v2

--- app/Http/Controllers/censo_controller_v2.php
+++ app/Http/Controllers/censo_controller_v2.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\censo_company_v2;
 use App\Models\censo_status_v2;
 use Illuminate\Http\Request;
 
 class censo_controller_v2 extends Controller
 {
     protected $connection = 'DevCenso';
+
     public function ctl_status()
     {
         $ctl_status = censo_status_v2::all();
@@ -15,6 +17,16 @@ class censo_controller_v2 extends Controller
             'status' => true,
             'message' => 'Successful query.',
             'data' => $ctl_status
+        ], 200);
+    }
+
+    public function ctl_company()
+    {
+        $ctl_company = censo_company_v2::all();
+        return response()->json([
+            'status' => true,
+            'message' => 'Successful query.',
+            'data' => $ctl_company
         ], 200);
     }
 }
